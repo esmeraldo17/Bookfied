1e7464cc4248a86a404da9d7e399dbbf383c49eb
esmeraldo17 <esmeraldoalberto55@gmail.com>
2026-03-13 18:07:16 +0100

    feat: heroSection and Book card implementacion, add all types in types files and a mockup of book at /lib/constants file

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import HeroSection from '@/components/HeroSection'
+import BookCard from '@/components/BookCard'
+import { sampleBooks } from '@/lib/constants'
+
+
 
 const page = () => {
   return (
-    <div>
-      
-    </div>
+    <main className="wrapper container">
+      <HeroSection />
+
+      <div className="library-books-grid">
+        {sampleBooks.map((book) => (
+          <BookCard key={book._id} title={book.title} author={book.author} coverURL={book.coverURL} slug={book.slug} />
+        ))}
+      </div>
+    </main>
   )
 }
 
